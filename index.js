0aabae72b4c7132c24e864a0986fbf2915a24663
Alaq A.Ryanta <message.driyant@outlook.com>
2025-06-19 20:13:41 +0700

    feat: add endpoint generate-text

--- index.js
+++ index.js
@@ -11,12 +11,27 @@ const app = express();
 app.use(express.json());
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
-const model = genAI.getGenerativeModel({ model: "models/gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "models/gemini-2.5-flash" });
 
 const upload = multer({ dest: "uploads/" });
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Gemini AI App is running" });
+});
+
+app.post("/generate-text", async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    if (!prompt) {
+      return res.status(400).json({ error: "Prompt is required" });
+    }
+    const result = await model.generateContent(prompt);
+    const resp = result.response;
+    res.status(200).json({ output: resp.text() });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error });
+  }
 });
 
 app.listen(port, () => {
